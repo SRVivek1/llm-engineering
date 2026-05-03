7fac6114bbc78e10995b75d8a486d9671299f057
Vivek Singh <16172793+SRVivek1@users.noreply.github.com>
2026-05-03 22:26:32 +0530

    env setup - No API key

--- c1-lang-chain-framework/c1-t2-hello-world-app/main.py
+++ c1-lang-chain-framework/c1-t2-hello-world-app/main.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Load env variables
+load_dotenv()
+
 def main():
     print("Hello from c1-t2-hello-world-app!")
-
+    print(f"API Key: {os.getenv('OPENAI_API_KEY')}")
 
 if __name__ == "__main__":
     main()
